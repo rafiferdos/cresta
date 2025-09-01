0b4601c1e07768656f64331ec7198310bf9d1006
Rafi Ferdos <rafiferdos@gmail.com>
2025-09-01 12:11:27 +0600

    refactor: remove unused components and utility functions
    - Deleted Navbar component and its associated styles.
    - Removed Avatar, Badge, Button, Card, Input, NavigationMenu components.
    - Eliminated item-related functions and MongoDB connection utilities.
    - Cleaned up utility functions for class name merging.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
-import { Navbar } from '@/components/navbar'
-import { AbstractBackground } from '@/components/abstract-background'
+
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -29,9 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased relative`}
       >
-        <AbstractBackground />
         <div className='relative z-10'>
-          <Navbar />
           {children}
         </div>
       </body>
